38f18b45c3ad1a5ee01d02fb76bf6c92f2094519
adiv1 <30501822+adiv1@users.noreply.github.com>
2023-09-16 21:06:17 +0530

    1 file modified

--- src/app/services/standings.service.ts
+++ src/app/services/standings.service.ts
@@ -4,22 +4,24 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { League } from '../models/league';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StandingsService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  private _leagues: Subject<Array<League>> = new BehaviorSubject<Array<League>>([]);
+  private _leagues: Subject<Array<League>> = new BehaviorSubject<Array<League>>(
+    []
+  );
   // private apiKey = '89e95f39f83f976374c0ca4447ff12ac__';
   // private apiKey = '59c374a599ebc97a6f6b6ec5fa2dd68e';
   //  private apiKey = '7a34be7e65ac0b9ecd709b95fe683e33';
-   private apiKey = 'af1fb7cc8a8645f9378caadbcb6b3461';
+  //  private apiKey = 'af1fb7cc8a8645f9378caadbcb6b3461';
   private API = 'https://v3.football.api-sports.io';
   private readonly currentYear: number = new Date().getFullYear();
-  public readonly leagues: Observable<Array<League>> = this._leagues.asObservable();
+  public readonly leagues: Observable<Array<League>> =
+    this._leagues.asObservable();
 
-  private readonly leagueId : any = {
+  private readonly leagueId: any = {
     england: 39,
     spain: 107,
     germany: 78,
@@ -29,46 +31,45 @@ export class StandingsService {
   getStandings(
     league: string | number,
     season: string = this.currentYear.toString()
-  ): Observable <League[]>{
+  ): Observable<League[]> {
     const headers = new HttpHeaders({
       'x-rapidapi-host': 'v3.football.api-sports.io',
       'x-rapidapi-key': this.apiKey,
     });
-    let reqParam = typeof(league) == 'string' ? this.leagueId[league] : league;
+    let reqParam = typeof league == 'string' ? this.leagueId[league] : league;
     return this.http
-    .get<League[]>( `${this.API}/standings?league=${reqParam}&season=${season}`,
-      { headers }
-    )
-    .pipe(
-      map((response) => {
-        return response;
-      })
-    );
+      .get<League[]>(
+        `${this.API}/standings?league=${reqParam}&season=${season}`,
+        { headers }
+      )
+      .pipe(
+        map((response) => {
+          return response;
+        })
+      );
   }
 
   getStandingsJson() {
-    return this.http.get('/assets/response.json')
+    return this.http.get('/assets/response.json');
   }
 
   getTopListTeams(
-      teamId: Number,
-      season: string = this.currentYear.toString()
-    ) {
-      const headers = new HttpHeaders({
-        'x-rapidapi-host': 'v3.football.api-sports.io/fixtures',
-        'x-rapidapi-key': this.apiKey,
-      });
-  
-      return this.http
-        .get(`${this.API}/fixtures?team=${teamId}&season=${season}&last=10`, {
-          headers,
+    teamId: Number,
+    season: string = this.currentYear.toString()
+  ) {
+    const headers = new HttpHeaders({
+      'x-rapidapi-host': 'v3.football.api-sports.io/fixtures',
+      'x-rapidapi-key': this.apiKey,
+    });
+
+    return this.http
+      .get(`${this.API}/fixtures?team=${teamId}&season=${season}&last=10`, {
+        headers,
+      })
+      .pipe(
+        map((response) => {
+          return response;
         })
-        .pipe(
-          map((response) => {
-            return response;
-          })
-        );
-    }
-
-
+      );
+  }
 }
